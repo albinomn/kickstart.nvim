bda03ac6de7fead6d26d7b0784ee7ff944deb7ca
Albino Neto <albinomeneguetti@hotmail.com.br>
2023-08-17 01:00:11 -0300

    kickstart 2

--- init.lua
+++ init.lua
@@ -316,7 +316,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'elixir', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -439,7 +439,13 @@ end
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {
+    settings = {
+      gopls = {
+          gofumpt = true
+      }
+    }
+  },
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
